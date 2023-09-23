00d1e19e7a88c496d86b5c94510217551152a1ef
Siddharth Bhat <dbgt4531@gmail.com>
2023-09-23 21:13:58 +0530

    login ui done

--- src/utils/Routers.jsx
+++ src/utils/Routers.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from '../components/Login';
-import Home from '../components/Home';
+import Login from '../components/login/Login';
+import Home from '../components/home/Home';
 
 const Routers = () => {
     return (
