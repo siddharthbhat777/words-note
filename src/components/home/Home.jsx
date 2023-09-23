4a01caaa44ec2350fac11fd44fcbd2ef38eb67e4
Siddharth Bhat <dbgt4531@gmail.com>
2023-09-23 23:27:58 +0530

    created homescreen UI layout

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-// import classes from './Home.module.css';
+import classes from './Home.module.css';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className={classes.homeBackground}>
+            <div className={classes.homeActivityContainer}>
+                <div className={classes.topContainer}>
+                    <div className={classes.addWordContainer}>
+                        <input type='text' className={classes.addWord} placeholder='Enter your point to remember' />
+                        <hr className={classes.topHr} />
+                    </div>
+                </div>
+                <div className={classes.bottomContainer}>
+                    <div className={classes.activityListContainer}></div>
+                </div>
+            </div>
+        </div>
     );
 };
 
