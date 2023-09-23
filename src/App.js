7850ec4e3e40d02406aff7bca5e8e8e928487894
Siddharth Bhat <dbgt4531@gmail.com>
2023-09-24 01:49:41 +0530

    home done and dark mode working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Routers from './utils/Routers';
+import classes from './App.module.css';
 
 const App = () => {
-  /* const [darkModeOn, setDarkModeOn] = useState(false);
+  const [darkModeOn, setDarkModeOn] = useState(false);
   const toggleDarkMode = () => {
     setDarkModeOn(!darkModeOn);
+  };
+
+  useEffect(() => {
     document.body.classList.toggle('darkMode', darkModeOn);
-  }; */
+  }, [darkModeOn])
 
   return (
     <div>
-      {/* <button className='check' onClick={toggleDarkMode}>Do it</button> */}
+      <button className={classes.darkModeButton} onClick={toggleDarkMode}>Do it</button>
       <Routers />
     </div>
   );
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import classes from './Home.module.css';
 
 const Home = () => {
+    const handleUpdate = () => {
+        console.log('update working');
+    };
+    const handleDelete = () => {
+        console.log('delete working');
+    };
     return (
         <div className={classes.homeBackground}>
             <div className={classes.homeActivityContainer}>
@@ -12,7 +18,98 @@ const Home = () => {
                     </div>
                 </div>
                 <div className={classes.bottomContainer}>
-                    <div className={classes.activityListContainer}></div>
+                    <div className={classes.activityListContainer}>
+                        <div className={classes.singleElement}>
+                            <span className={classes.activityName}>My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj</span>
+                            <div className={classes.rightElements}>
+                                <span className={classes.activityDateTime}>12/12/2023</span>
+                                <div className={classes.modifyIcons}>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleUpdate} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                    </svg>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleDelete} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.singleElement}>
+                            <span className={classes.activityName}>My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj</span>
+                            <div className={classes.rightElements}>
+                                <span className={classes.activityDateTime}>12/12/2023</span>
+                                <div className={classes.modifyIcons}>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleUpdate} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                    </svg>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleDelete} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.singleElement}>
+                            <span className={classes.activityName}>My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj</span>
+                            <div className={classes.rightElements}>
+                                <span className={classes.activityDateTime}>12/12/2023</span>
+                                <div className={classes.modifyIcons}>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleUpdate} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                    </svg>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleDelete} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.singleElement}>
+                            <span className={classes.activityName}>My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj</span>
+                            <div className={classes.rightElements}>
+                                <span className={classes.activityDateTime}>12/12/2023</span>
+                                <div className={classes.modifyIcons}>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleUpdate} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                    </svg>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleDelete} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.singleElement}>
+                            <span className={classes.activityName}>My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj</span>
+                            <div className={classes.rightElements}>
+                                <span className={classes.activityDateTime}>12/12/2023</span>
+                                <div className={classes.modifyIcons}>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleUpdate} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                    </svg>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleDelete} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.singleElement}>
+                            <span className={classes.activityName}>My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj My ComplaintMy ComplaintMy Complaint Complaint Complaintygfchj</span>
+                            <div className={classes.rightElements}>
+                                <span className={classes.activityDateTime}>12/12/2023</span>
+                                <div className={classes.modifyIcons}>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleUpdate} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                    </svg>
+                                    <svg style={{ cursor: 'pointer' }} onClick={handleDelete} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
