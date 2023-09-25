7900f100b1444f3e0e4dccd0ac066a00f96e838e
Siddharth Bhat <dbgt4531@gmail.com>
2023-09-25 23:24:27 +0530

    added button

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -13,7 +13,14 @@ const Home = () => {
             <div className={classes.homeActivityContainer}>
                 <div className={classes.topContainer}>
                     <div className={classes.addWordContainer}>
-                        <input type='text' className={classes.addWord} placeholder='Enter your point to remember' />
+                        <div className={classes.addWordLayout}>
+                            <input type='text' className={classes.addWord} placeholder='Enter your point to remember' />
+                            <button className={classes.addWordButton}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                                </svg>
+                            </button>
+                        </div>
                         <hr className={classes.topHr} />
                     </div>
                 </div>
