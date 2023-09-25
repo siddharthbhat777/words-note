1dad8d56127e1ad46ca9de6baa5f46f2c09ceb89
Siddharth Bhat <dbgt4531@gmail.com>
2023-09-25 22:58:22 +0530

    added firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from '@firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBzxg6PJ4GCMitS4lTQ31h0fW4szFtk9I4",
+    authDomain: "words-note-b8d20.firebaseapp.com",
+    projectId: "words-note-b8d20",
+    storageBucket: "words-note-b8d20.appspot.com",
+    messagingSenderId: "327180999104",
+    appId: "1:327180999104:web:57fbf88b13f258c6e19025",
+    measurementId: "G-4X749HPNDM"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
