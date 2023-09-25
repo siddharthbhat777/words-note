b6f3caa7007b66815e03a5abea74289effc37cd3
Siddharth Bhat <dbgt4531@gmail.com>
2023-09-25 23:51:16 +0530

    setup on login click

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
@@ -12,5 +13,17 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
+    });
+};
 
 export const db = getFirestore(app);
