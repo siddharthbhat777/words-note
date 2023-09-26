cf0cd2dd261aab92b44a1ba578bf4eb08b7dd1c4
Siddharth Bhat <dbgt4531@gmail.com>
2023-09-26 23:22:00 +0530

    editing of message done

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import classes from './Home.module.css';
 import { useNavigate } from 'react-router-dom';
-import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { addDoc, collection, doc, getDocs, updateDoc } from 'firebase/firestore';
 import { db } from '../../firebase-config';
 import Loader from '../loader/Loader';
 
@@ -12,8 +12,11 @@ const Home = () => {
     const navigate = useNavigate();
     const messageTextRef = useRef();
     const [words, setWords] = useState([]);
+    const [editItemId, setEditItemId] = useState(null);
     const [showLoader, setShowLoader] = useState(false);
     const [refreshList, setRefreshList] = useState(false);
+    const [editedText, setEditedText] = useState('');
+
 
     const sortedData = React.useMemo(() => { return [...words].sort((a, b) => new Date(b.date.seconds) - new Date(a.date.seconds)) }, [words]);
 
@@ -54,8 +57,11 @@ const Home = () => {
         navigate('/login');
     };
 
-    const handleUpdate = () => {
-        console.log('update working');
+    const handleEdit = async (id) => {
+        const wordDoc = doc(db, userEmail, id);
+        await updateDoc(wordDoc, { message: editedText });
+        setEditItemId(null);
+        setRefreshList(true);
     };
 
     const handleDelete = () => {
@@ -120,17 +126,30 @@ const Home = () => {
                                 :
                                 sortedData.map((word) => (
                                     <div className={classes.singleElement} key={word.id}>
-                                        <span className={classes.activityName}>{word.message}</span>
+                                        <span className={`${classes.activityName} ${editItemId === word.id ? classes.hideLayout : ''}`}>{word.message}</span>
+                                        <div className={editItemId === word.id ? '' : classes.hideLayout}>
+                                            <div className={classes.activityNameEdit}>
+                                                <textarea defaultValue={word.message} className={classes.activityName} onChange={(e) => setEditedText(e.target.value)} />
+                                                <div className={classes.editButtonsLayout}>
+                                                    <button className={classes.editButton} onClick={() => handleEdit(word.id)}>Edit</button>
+                                                    <button className={classes.editCancelButton} onClick={() => setEditItemId(null)}>Cancel</button>
+                                                </div>
+                                            </div>
+                                        </div>
                                         <div className={classes.rightElements}>
                                             <span className={classes.activityDateTime}>{getFormattedDate(word.date)}</span>
                                             <div className={classes.modifyIcons}>
-                                                <svg style={{ cursor: 'pointer' }} onClick={handleUpdate} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
-                                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                                    <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                                                </svg>
-                                                <svg style={{ cursor: 'pointer' }} onClick={handleDelete} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
-                                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
-                                                </svg>
+                                                <div className={classes.editMessage} onClick={() => setEditItemId(word.id)}>
+                                                    <svg style={{ cursor: 'pointer' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                                    </svg>
+                                                </div>
+                                                <div className={classes.deleteMessage} onClick={handleDelete}>
+                                                    <svg style={{ cursor: 'pointer' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                                                    </svg>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
